6f3e754babdbb7baa7674c028dd90ddb30d85108
Alisher <alishcashq@gmail.com>
2022-05-29 21:19:39 +0600

    I'm alive

--- js.js
+++ js.js
@@ -1,26 +1,23 @@
 let btnGenerator = document.querySelector(".btn-generate");
 let ticketWrapper = document.querySelector(".tickets-wrapper");
+let basket = document.querySelector('.basket')
 let limit = 46;
-let ticket = document.querySelectorAll(".ticket");
-let numActive = document.querySelectorAll(".active").length;
-// let ticketNumList = document.querySelectorAll('.ticket-numlist');
 let numCount = 6;
-let biletCount = 1;
-let outTicketNumber = document.querySelector(".out-ticket-number");
-let arrayTicketNumber = [];
+let biletCount = 0;
+// let arrayTicketNumber = [];
 
 btnGenerator.addEventListener("click", () => {
-  // biletCount++;
+  ticketGenerate();
+});
 
-  ticketWrapper.innerHTML += `<div class="out-ticket-number"></div><div class="ticket"><h1 class="ticket-count">${
-    biletCount + 1
-  } Билет</h1><div class="ticket-prgoress-wrapper">
+// генерация билета
+
+const ticketGenerate = () => {
+  ticketWrapper.innerHTML += `<div class="ticket"><h1 class="ticket-count">${++biletCount} Билет</h1><div class="out-ticket-number"></div><div class="ticket-prgoress-wrapper">
     <div class="ticket-progress"></div>
 </div><div class="ticket-numlist"></div></div>`;
-
   let ticketNumList = document.querySelectorAll(".ticket-numlist");
   let ticket = document.querySelectorAll(".ticket");
-
   ticketNumList.forEach((elem) => {
     if (elem.innerHTML == "") {
       for (let i = 1; i <= limit; i++) {
@@ -28,33 +25,79 @@ btnGenerator.addEventListener("click", () => {
       }
     }
   });
-
-  // let tikcetsNum = document.querySelectorAll(".ticket-num");
-
-  ticket.forEach((el) => {
-    // let arrayTicketNumber = [];
-    // let outTicketNumberq = el.querySelectorAll(".out-ticket-number");
-    let tikcetsNumq = el.querySelectorAll(".ticket-num");
-    tikcetsNumq.forEach((elem) => {
-      elem.addEventListener("click", () => {
-        elem.classList.toggle("active");
-
-        numArrayPush(elem, arrayTicketNumber);  
-
-        console.log(arrayTicketNumber);
-        let numActive = el.querySelectorAll(".active").length;
-        progress(el, numActive);
-        // console.log(numActive);
-console.log(outTicketNumber)
-        outTicketNumber.innerHTML = arrayTicketNumber;
-      });
-    });
-  });
-});
-window.onscroll = () => {
-  // console.log(window.pageYOffset)
+  if(ticket.length === 3) {
+    document.body.style.background = '#ffeb3b'
+  }
+  ticketClick(ticket);
 };
 
+
+function ticketClick(ticket, massiv) {
+
+  ticket.forEach((el) => {
+    let tikcetsNumq = el.querySelectorAll(".ticket-num");
+
+    let outTicketNumber = el.querySelector(".out-ticket-number");
+    massiv = [];
+
+    tikcetsNumq.forEach((elem) => {
+
+      elem.addEventListener("click", () => {
+        elem.classList.toggle("active");
+        let numActive = el.querySelectorAll(".active").length;
+
+        progress(el, numActive);
+        numArrayPush(elem, massiv);
+        outNumber(massiv,outTicketNumber)
+        console.log(massiv);
+        console.log(outTicketNumber);
+        // outTicketNumber.innerHTML = massiv;
+      });
+
+    });
+  });
+}
+
+const outNumber = (massiv,outTicketNumber) => {
+
+  outTicketNumber.innerHTML = ``
+  massiv.sort((a,b) => a -b)
+   for(let key in massiv) {
+     console.log(massiv[key])
+     outTicketNumber.innerHTML += `<div class="out-number">${massiv[key]}</div>`
+
+   }
+
+}
+
+
+//  Клики блики
+// function ticketClick(ticket) {
+
+//   ticket.forEach((el) => {
+//     let tikcetsNumq = el.querySelectorAll(".ticket-num");
+
+//     let outTicketNumber = el.querySelector(".out-ticket-number");
+//   let arrayTicketNumber = [];
+
+//     tikcetsNumq.forEach((elem) => {
+
+//       elem.addEventListener("click", () => {
+//         elem.classList.toggle("active");
+//         let numActive = el.querySelectorAll(".active").length;
+
+//         progress(el, numActive);
+//         numArrayPush(elem, arrayTicketNumber);
+
+//         console.log(arrayTicketNumber);
+//         console.log(outTicketNumber);
+//         outTicketNumber.innerHTML = arrayTicketNumber;
+//       });
+//     });
+//   });
+// }
+
+ticketGenerate();
 const progress = (el, numActive) => {
   let progressLine = el.querySelectorAll(".ticket-progress");
   let progressP = 100 / (numCount / numActive);
@@ -63,14 +106,12 @@ const progress = (el, numActive) => {
   });
 };
 
-const numArrayPush = (elem, array) => {
+function numArrayPush(elem, array) {
   let btnValue = elem.innerHTML;
   const index = array.indexOf(btnValue);
   if (elem.classList.contains("active")) {
-    arrayTicketNumber.push(elem.innerHTML);
+    array.push(elem.innerHTML);
   } else {
-    arrayTicketNumber.splice(index, 1);
+    array.splice(index, 1);
   }
-  console.log(index + " значение");
-  console.log(arrayTicketNumber.indexOf());
-};
+}
