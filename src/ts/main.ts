c576f79237216dd5444edddc75e94927f2f676b2
Alisher <alishcashq@gmail.com>
2022-08-01 21:08:13 +0600

    upd

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,88 +1,72 @@
-import { basketSelectors } from "./components/basket/basket-selectors";
 import generateTicketNumbers from "./components/create-ticket/generate-ticket-numbers";
 import ticketGenerate from "./components/create-ticket/ticket-generate";
-import { limitCheck } from "./components/ticket-logyc/limit-active-number";
-import progress from "./components/ticket-logyc/progress-line";
+import {limitCheck} from "./components/ticket-logyc/limit-active-number";
+import progressLine from "./components/ticket-logyc/progress-line";
 import ticketAutofill from "./components/ticket-logyc/ticket-auto-fill";
-import {
-  outNumber,
-  numArrayPush,
-} from "./components/ticket-logyc/ticket-out-number";
+import {outNumber, numArrayPush} from "./components/ticket-logyc/ticket-out-number";
 
-let addTicketBtn = document.querySelector(".btn-generate") as HTMLElement;
+let btnGenerator = document.querySelector(".btn-generate") as HTMLElement;
 let ticketOutWrapper = document.querySelector(
   ".tickets-out-wrapper"
 ) as HTMLElement;
+let basket = document.querySelector(".basket") as HTMLElement;
+let amountNumber: number = 46;
+let limitNumber: number = 6;
 let biletCount: number = 0;
+let basketText = document.querySelector(".basket-text") as HTMLElement;
+let basketContent = document.querySelector(
+  ".basket-hidden-content"
+) as HTMLElement;
+let basketPrice = document.querySelector(".basket-ticket-price") as HTMLElement;
 let basketObj = {};
 let filterTicket = document.querySelector(".filter-tickets") as HTMLElement;
-let basketBtn = document.querySelector('.basket-btn') as HTMLElement;
+let ticketPrice: number = 200;
+// let arrayTicketNumber = [];
 
 let gameConfig = {
   ticketPrice: 200,
   limitNumber: 6,
-  amountNumber: 46,
-};
-
-// document.body.onclick = () => {
-
-// }
-basketBtn.onclick = () => {
-  console.log('123')
+  amountNumber: 46,  
 }
-addTicketBtn.addEventListener("click", () => {
+
+
+btnGenerator.addEventListener("click", () => {
   ticketGenerate(
     ticketOutWrapper,
     ++biletCount,
-    generateTicketNumbers(gameConfig.amountNumber)
+    generateTicketNumbers(amountNumber)
   );
   ticketClick();
-
-  let ticket = document.querySelectorAll(".ticket");
-  ticket[ticket.length - 1].scrollIntoView();
 });
 
 // Клики по цифрам внутри определенного билета
 function ticketClick() {
   let ticket = document.querySelectorAll(".ticket");
   ticket.forEach((ticket, index) => {
-    let tikcetsNum = ticket.querySelectorAll(".ticket-num");
-    let ticketBtnAutoFill = ticket.querySelector(".ticket-autofill");
-    let outTicketNumber = ticket.querySelector(".out-ticket-number");
-    let ticketRemove = ticket.querySelector(".ticket-remove");
-    ticket["array"] = [];
+    let tikcetsNum = ticket.querySelectorAll<HTMLButtonElement>(".ticket-num") ;
+    let ticketBtnAutoFill = ticket.querySelector(".ticket-autofill") as HTMLButtonElement;
+    let outTicketNumber = ticket.querySelector(".out-ticket-number") as HTMLElement;
+    let ticketRemove = ticket.querySelector(".ticket-remove") as HTMLElement;
+    ticket[index] = [];
 
     tikcetsNum.forEach((ticketsNumBtn) => {
       ticketsNumBtn.addEventListener("click", () => {
-        ticketsNumBtn.classList.toggle("ticket-num--active");
-        let numActive = ticket.querySelectorAll(".ticket-num--active").length;
+        ticketsNumBtn.classList.toggle("active");
+        let numActive = ticket.querySelectorAll(".active").length;
 
         console.log(ticket[index]);
-        limitCheck(numActive, tikcetsNum, ticket, gameConfig);
-        progress(ticket, numActive, gameConfig.limitNumber);
-        numArrayPush(ticketsNumBtn, ticket["array"]);
-        outNumber(ticket["array"], outTicketNumber);
+        limitCheck(numActive, tikcetsNum, ticket,limitNumber);
+        progressLine(ticket, numActive, limitNumber);
+        numArrayPush(ticketsNumBtn, ticket[index]);
+        outNumber(ticket[index], outTicketNumber);
         ticketPriceAmount(ticket, index);
       });
     });
 
-    ticketAutofill(
-      ticketBtnAutoFill,
-      ticket,
-      tikcetsNum,
-      6,
-      gameConfig.amountNumber
-    );
-    ticketRemoveF(ticketRemove, ticket,index);
+    ticketAutofill(ticketBtnAutoFill, ticket, tikcetsNum,limitNumber,amountNumber);
+    ticketRemoveF(ticketRemove, ticket);
   });
 }
-
-basketSelectors();
-let basketText = document.querySelector(".basket-text") as HTMLElement;
-let basketContent = document.querySelector(
-  ".basket-hidden-content"
-) as HTMLElement;
-let basketPrice = document.querySelector(".basket-ticket-price") as HTMLElement;
 // Счетчик суммы в баскете
 const ticketPriceAmount = (ticket, index) => {
   let summ: number = 0;
@@ -91,9 +75,12 @@ const ticketPriceAmount = (ticket, index) => {
   ).length;
 
   if (ticket.dataset.ticketComplete === "true") {
-    basketObj[index] = gameConfig.ticketPrice;
+    basketObj[index] = ticketPrice;
+    basketObj[index + 'Числа'] = '123'
+
     if (ticket.dataset.bigStavka !== "false") {
-      basketObj[index] = ticket.dataset.bigStavka * 9 * gameConfig.ticketPrice;
+      basketObj[index] = ticket.dataset.bigStavka * 9 * ticketPrice;
+      basketObj[index + 'Числа'] = ticket.querySelectorAll('')
     }
   } else {
     delete basketObj[index];
@@ -107,7 +94,7 @@ const ticketPriceAmount = (ticket, index) => {
 
   console.log(basketObj);
 
-  basketPrice.textContent = `Сумма: ${summ.toString()}`;
+  basketPrice.textContent = summ.toString();
 
   if (ticketComplete >= 1) {
     basketText.innerHTML = "Начнем же ?";
@@ -118,44 +105,33 @@ const ticketPriceAmount = (ticket, index) => {
   }
 };
 
+
 // Удалить билет
-const ticketRemoveF = (ticketRemove, ticket,index) => {
-  let tickets = document.querySelectorAll<HTMLElement>(".ticket");
+const ticketRemoveF = (ticketRemove, ticket) => {
+  let tickets = document.querySelectorAll(".ticket");
+
   ticketRemove.addEventListener("click", () => {
     ticket.remove();
-    console.log(index,'index')
-    delete basketObj[index]
-    console.log(basketObj);
-    ticketPriceAmount(ticket, index)
-    tickets = document.querySelectorAll(".ticket");
     tickets.forEach((e, index) => {
       let count = e.querySelector(".ticket-count");
-      count.innerHTML = ++index + " Билет";
-      biletCount = index
+      count.innerHTML = index++ + " Билет";
+      console.log();
+      console.log(e);
     });
-    console.log(tickets.length);
-    // if (tickets.length === 1) {
-    //   console.log("last");
-    //   tickets[0].querySelector<HTMLElement>(".ticket-remove").style.display =
-    //     "none";
-    // } else {
-    //   tickets[0].querySelector<HTMLElement>(".ticket-remove").style.display =
-    //     "block";
-    //   console.log("321");
-    // }
+    let ticketq = document.querySelectorAll(".ticket");
+    ticketq.forEach((tickets) => {
+      let counter = tickets.querySelector(".ticket-count");
+    });
   });
 };
 
+
+// window.onscroll = ()=> {
+//   if(window.pageYOffset > 200) {
+//     basket.style.cssText = `position: fixed; right: 0`
+//   }}
 for (let i = 0; i < 3; i++) {
-  ticketGenerate(
-    ticketOutWrapper,
-    ++biletCount,
-    generateTicketNumbers(gameConfig.amountNumber)
-  );
-  // невероятная оптимизация
-  if (i === 2) {
-    ticketClick();
-  }
+  btnGenerator.click();
 }
 
 filterTicket.addEventListener("click", () => {
