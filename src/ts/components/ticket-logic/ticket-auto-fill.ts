6f2235cd210a4e789a6b231716c00b2895045596
Alisher <alishcashq@gmail.com>
2022-08-27 02:14:05 +0600

    la

--- src/ts/components/ticket-logic/ticket-auto-fill.ts
+++ src/ts/components/ticket-logic/ticket-auto-fill.ts
@@ -1,16 +1,18 @@
 // Собрать билет
-export const ticketAutofill = (ticketBtnAutoFill, tikcetsNum,ticket) => {
-    ticketBtnAutoFill.addEventListener("click", () => {
-      let randomArr = Array(tikcetsNum.length).fill(0).map((item, i) => ++i);
-      const win = Array(parseInt(ticket.dataset.limitNumber)).fill(0).map(() => randomArr.splice(Math.floor(Math.random() * randomArr.length), 1).pop())
-console.log(ticket.dataset.limitNumber)
-      tikcetsNum.forEach((ticketsNumBtn) => {
-          ticketsNumBtn.classList.remove("ticket-num--active");
-          ticketsNumBtn.removeAttribute('disabled')
-      });
-      for(let key of win) {
-        console.log(key)
-        tikcetsNum[key - 1].click()
-      }
-    });
-  };
+export const ticketAutofill = (ticketBtnAutoFill, tikcetsNum, ticket) => {
+  ticketBtnAutoFill.addEventListener("click", () => {
+    let randomArr = Array(tikcetsNum.length)
+      .fill(0)
+      .map((item, i) => ++i)
+    let randomArrClick = Array(parseInt(ticket.dataset.limitNumber))
+      .fill(0)
+      .map(() => randomArr.splice(Math.floor(Math.random() * randomArr.length), 1).pop())
+    tikcetsNum.forEach((ticketsNumBtn) => {
+      ticketsNumBtn.classList.remove("ticket-num--active")
+      ticketsNumBtn.removeAttribute("disabled")
+    })
+    for (let key of randomArrClick) {
+      tikcetsNum[key - 1].click()
+    }
+  })
+}
